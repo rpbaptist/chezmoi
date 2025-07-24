d6d86a21957d3f3645fecd1759cb43eabe17263d
Richard Baptist <code@richardbaptist.nl>
2025-07-24 09:30:40 +0200

    ergoterm iex tests

--- private_dot_config/nvim/lua/plugins/ergoterm.lua
+++ private_dot_config/nvim/lua/plugins/ergoterm.lua
@@ -2,4 +2,73 @@ return {
 	"waiting-for-dev/ergoterm.nvim",
 	event = "VeryLazy",
 	opts = {},
+	keys = function()
+		local terms = require("ergoterm.terminal")
+
+		local iex_tests = terms.Terminal:new({
+			cmd = "iex -S mix",
+			name = "IexTests",
+			layout = "right",
+			selectable = false,
+			env = {
+				MIX_ENV = "test",
+			},
+		})
+
+		return {
+			{
+				"<leader>tg",
+				function()
+					iex_tests.toggle()
+				end,
+			},
+			{
+				"<leader>tf",
+				function()
+					iex_tests.start()
+					local file_path = vim.fn.expand("%")
+					iex_tests:send({ 'IexTests.test("' .. file_path .. '")' }, { action = "visible" })
+				end,
+			},
+			{
+				"<leader>tt",
+				function()
+					iex_tests.start()
+					local file_path = vim.fn.expand("%")
+					local line_number = vim.fn.line(".")
+					iex_tests:send(
+						{ 'IexTests.test("' .. file_path .. ":" .. line_number .. '")' },
+						{ action = "visible" }
+					)
+				end,
+			},
+			{
+				"<leader>twf",
+				function()
+					iex_tests.start()
+					local file_path = vim.fn.expand("%")
+					iex_tests:send({ 'IexTests.test_watch("' .. file_path .. '")' }, { action = "visible" })
+				end,
+			},
+			{
+				"<leader>twt",
+				function()
+					iex_tests.start()
+					local file_path = vim.fn.expand("%")
+					local line_number = vim.fn.line(".")
+					iex_tests:send(
+						{ 'IexTests.test_watch("' .. file_path .. ":" .. line_number .. '")' },
+						{ action = "visible" }
+					)
+				end,
+			},
+			{
+				"<leader>tws",
+				function()
+					iex_tests.start()
+					iex_tests:send({ "IexTests.stop_watch()" }, { action = "hidden" })
+				end,
+			},
+		}
+	end,
 }
