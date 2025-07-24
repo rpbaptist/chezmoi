0b777b68beaadcd6b0b683b83a771dda9639250b
Richard Baptist <code@richardbaptist.nl>
2025-07-24 09:29:56 +0200

    Update mini-surround

--- private_dot_config/nvim/lua/plugins/mini-surround.lua
+++ private_dot_config/nvim/lua/plugins/mini-surround.lua
@@ -1,6 +1,6 @@
 return {
 	"echasnovski/mini.surround",
-  event = "VeryLazy",
+	event = "VeryLazy",
 	opts = {
 		mappings = {
 			add = "gsa", -- Add surrounding in Normal and Visual modes
@@ -12,19 +12,4 @@ return {
 			update_n_lines = "gsn", -- Update `n_lines`
 		},
 	},
-	keys = function(_, keys)
-		local mappings = {
-			{ "gsa", desc = "Add Surrounding", mode = { "n", "v" } },
-			{ "gsd", desc = "Delete Surrounding" },
-			{ "gsf", desc = "Find Right Surrounding" },
-			{ "gsF", desc = "Find Left Surrounding" },
-			{ "gsh", desc = "Highlight Surrounding" },
-			{ "gsr", desc = "Replace Surrounding" },
-			{ "gsn", desc = "Update `MiniSurround.config.n_lines`" },
-		}
-		mappings = vim.tbl_filter(function(m)
-			return m[1] and #m[1] > 0
-		end, mappings)
-		return vim.list_extend(mappings, keys)
-	end,
 }
