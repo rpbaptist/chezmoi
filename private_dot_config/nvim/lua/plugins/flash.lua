e3d95344408fbf18e63fc968fcfe27407d826e1d
Richard Baptist <code@richardbaptist.nl>
2025-11-04 16:08:44 +0100

    Update config

--- private_dot_config/nvim/lua/plugins/flash.lua
+++ private_dot_config/nvim/lua/plugins/flash.lua
@@ -2,10 +2,12 @@ return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
 	opts = {
-    multi_window = false,
-    jump = {
-      nohlsearch = true
-    },
+		search = {
+			multi_window = false,
+		},
+		jump = {
+			nohlsearch = true,
+		},
 		modes = {
 			search = {
 				enabled = true,
@@ -18,7 +20,6 @@ return {
     { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     -- Simulate nvim-treesitter incremental selection
     { "<c-space>", mode = { "n", "o", "x" },
       function()
