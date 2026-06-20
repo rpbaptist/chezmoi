7ea464c3abc859650ea955d776b7065e4d3337c0
Richard Baptist <code@richardbaptist.nl>
2026-06-20 16:49:46 +0200

    Move nvim config to its own repo, pull in via chezmoi external

--- private_dot_config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
-require("config.options")
-require("config.lazy")
-
-require("config.keymap")
-require("config.autocmds")
-require("config.lsp")
-
-vim.cmd([[colorscheme gruvbox]])
--- private_dot_config/nvim/lsp/dexter.lua
+++
@@ -1,6 +0,0 @@
-return {
-  cmd = { 'dexter', 'lsp' },
-  root_markers = { '.dexter/dexter.db', '.dexter.db', 'mix.exs' },
-  filetypes = { "elixir", "eelixir", "heex", "surface", "livebook" },
-}
-
